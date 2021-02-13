9b2e9e84dbe64492f4763af023c38d98d36167e9
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-13 20:27:21 -0300

    Add model and db context EF

---
+++ ProductCategoriesMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCategoriesMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Prod_Cat;Integrated Security=True");
+        }
+    }
+}
--- ProductCategoriesMVC/Startup.cs
+++ ProductCategoriesMVC/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProductCategoriesMVC.Models;
 
 namespace ProductCategoriesMVC
 {
@@ -24,6 +25,7 @@ namespace ProductCategoriesMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
