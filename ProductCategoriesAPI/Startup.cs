9e45060ced0ead5aee7804056910ded285add7e3
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-13 22:12:29 -0300

    Category controller

--- ProductCategoriesAPI/Startup.cs
+++ ProductCategoriesAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ProductCategoriesAPI
 {
@@ -26,6 +27,7 @@ namespace ProductCategoriesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductCategories API", Version = "v1" }); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +37,10 @@ namespace ProductCategoriesAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductCategories API"); });
 
             app.UseHttpsRedirection();
 
